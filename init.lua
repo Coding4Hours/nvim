4578e3e8d87fc1634aa2d6f965576a547382b557
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-08-23 21:27:49 -0700

    a

--- init.lua
+++ init.lua
@@ -1,31 +1,30 @@
+vim.g.mapleader = " "
+
+
+
 --==============================================================================
 -- Mini.deps
 --==============================================================================
 
 local path_package = vim.fn.stdpath("data") .. "/site/"
 local mini_path = path_package .. "pack/deps/start/mini.nvim"
+
 if not vim.uv.fs_stat(mini_path) then
-  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  vim.cmd('echo "Installing `mini.nvim`..." | redraw')
   local clone_cmd = { "git", "clone", "--filter=blob:none", "https://github.com/echasnovski/mini.nvim", mini_path }
   vim.fn.system(clone_cmd)
-  vim.cmd("packadd mini.nvim | helptags ALL")
-  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+  vim.cmd('echo "Installed `mini.nvim`." | redraw')
 end
 
 require("mini.deps").setup({ path = { package = path_package } })
 
 local add, later = MiniDeps.add, MiniDeps.later
 
-
-
 later(function()
-  vim.g.mapleader = " "
   require("options")
   require("autocmds")
   require("keymaps")
 end)
-
-
 
 --==============================================================================
 -- Plugins
@@ -33,16 +32,17 @@ end)
 
 
 
-
--- Plugin List
 add("folke/snacks.nvim")
 add('dstein64/vim-startuptime')
-add('nvim-lua/plenary.nvim')
-add({ source = 'rose-pine/neovim', name = 'rose-pine' })
+add({ source = "rose-pine/neovim", name = "rose-pine" })
 add('saghen/blink.cmp')
-add('neovim/nvim-lspconfig')
-add('mason-org/mason.nvim')
-add('mason-org/mason-lspconfig.nvim')
+add({
+  source = 'mason-org/mason-lspconfig.nvim',
+  depends = {
+    'mason-org/mason.nvim',
+    'neovim/nvim-lspconfig'
+  }
+})
 add({
   source = 'nvim-treesitter/nvim-treesitter',
   checkout = 'main',
@@ -50,43 +50,29 @@ add({
 })
 
 
+
 later(function()
-  local is_something_shown = function()
-    if vim.fn.argc() > 0 then return true end
+  local get_first_line = function()
+    local lines = vim.api.nvim_buf_get_lines(0, 0, 1, true)
+    return lines[1] or ''
+  end
+  local is_something_shown = vim.fn.argc() > 0
+      or #vim.tbl_filter(function(id) return vim.fn.buflisted(id) == 1 end, vim.api.nvim_list_bufs()) > 1
+      or vim.bo.filetype ~= ''
+      or vim.api.nvim_buf_line_count(0) > 1
+      or #get_first_line() > 0
 
-    local listed_buffers = vim.tbl_filter(
-      function(buf_id) return vim.fn.buflisted(buf_id) == 1 end,
-      vim.api.nvim_list_bufs()
-    )
-    if #listed_buffers > 1 then return true end
-
-    if vim.bo.filetype ~= '' then return true end
-
-    local n_lines = vim.api.nvim_buf_line_count(0)
-    if n_lines > 1 then return true end
-    local first_line = vim.api.nvim_buf_get_lines(0, 0, 1, true)[1]
-    if string.len(first_line) > 0 then return true end
-
-    return false
+  if not is_something_shown and vim.fn.filereadable("Session.vim") == 1 then
+    vim.cmd("source Session.vim")
   end
 
-  local session_file = "Session.vim"
-  if not is_something_shown() then
-    if vim.fn.filereadable(session_file) == 1 then
-      vim.cmd("source " .. session_file)
-    end
-  end
-
-
-
-  require('rose-pine').setup({
+  require("rose-pine").setup({
     styles = {
       transparency = true,
     },
   })
-  vim.cmd.colorscheme('rose-pine')
+  vim.cmd.colorscheme "rose-pine"
 
-  require('blink.cmp').setup(require('configs.cmp'))
   require('mason').setup({})
   require('mason-lspconfig').setup(require('configs.mason'))
   require('nvim-treesitter').setup(require('configs.treesitter'))
@@ -94,5 +80,7 @@ later(function()
   vim.notify = require('mini.notify').make_notify()
   require("mini.statusline").setup()
   require("mini.pairs").setup()
+  require("mini.icons").setup()
   require("snacks").setup({ picker = require("configs.picker") })
+  require('blink.cmp').setup(require('configs.cmp'))
 end)
