13b74228cc82aaf711f6e6f18eb11e0528f92fed
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-08-28 21:12:24 -0700

    a

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 vim.g.mapleader = " "
-local vim = vim
 
 local path_package = vim.fn.stdpath('data') .. '/site/'
 local mini_path = path_package .. 'pack/deps/start/mini.nvim'
+
 if not vim.loop.fs_stat(mini_path) then
 	vim.cmd('echo "Installing `mini.nvim`" | redraw')
 	local clone_cmd = {
@@ -15,116 +15,119 @@ if not vim.loop.fs_stat(mini_path) then
 end
 
 require('mini.deps').setup({ path = { package = path_package } })
-
---==============================================================================
--- Plugins
---==============================================================================
-
 local add, later = MiniDeps.add, MiniDeps.later
 
 
-
+--==============================================================================
+-- Plugin Declarations
+--==============================================================================
 later(function()
-	add('nvim-lualine/lualine.nvim')
-	add('saghen/blink.cmp')
-	add('mason-org/mason-lspconfig.nvim')
-	add('mason-org/mason.nvim')
-	add("shortcuts/no-neck-pain.nvim")
 	add('neovim/nvim-lspconfig')
+	add('mason-org/mason.nvim')
+	add('mason-org/mason-lspconfig.nvim')
+	add('saghen/blink.cmp')
 
+	add('nvim-lualine/lualine.nvim')
 	add({ source = "rose-pine/neovim", name = "rose-pine" })
-	add({
-		source = 'nvim-treesitter/nvim-treesitter',
-		checkout = 'main',
-	})
-	local autocmd = vim.api.nvim_create_autocmd
-	local map = vim.keymap.set
+	add("shortcuts/no-neck-pain.nvim")
+	add({ source = 'nvim-treesitter/nvim-treesitter', checkout = 'main' })
 
 	--==============================================================================
-	-- Initialization
+	-- Core Settings
 	--==============================================================================
-	require("rose-pine").setup({
-		styles = {
-			transparency = true,
-		},
-	})
+	vim.loader.enable()
+	local opt = vim.opt
+
+	opt.clipboard = "unnamedplus"
+	opt.cmdheight = 0
+	opt.ignorecase = true
+	opt.inccommand = "split"
+	opt.laststatus = 3
+	opt.number = true
+	opt.relativenumber = true
+	opt.shiftwidth = 2
+	opt.showtabline = 2
+	opt.signcolumn = "yes"
+	opt.smartindent = true
+	opt.smoothscroll = true
+	opt.tabstop = 2
+	opt.undofile = true
+	opt.whichwrap:append("<>[]hl")
+
+	vim.diagnostic.config { virtual_text = true }
+
+	--==============================================================================
+	-- Keymaps
+	--==============================================================================
+	local map = function(modes, lhs, rhs, opts)
+		local options = { noremap = true, silent = true }
+		if opts then
+			options = vim.tbl_extend("force", options, opts)
+		end
+		vim.keymap.set(modes, lhs, rhs, options)
+	end
+
+	map({ "n", "x" }, "c", '"_c', { desc = "Cut without yanking" })
+	map({ "n", "x" }, "<leader>d", '"_d', { desc = "Delete without yanking" })
+	map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+	map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
+	map("n", "<C-d>", "<C-d>zz", { desc = "Page down and center" })
+	map("n", "<C-u>", "<C-u>zz", { desc = "Page up and center" })
+	map({ 'n', 'v' }, 'B', '0', { noremap = true, desc = "Go to beginning of line" })
+	map({ 'n', 'v' }, 'E', '$', { noremap = true, desc = "Go to end of line" })
+	map("n", "n", "nzzzv", { desc = "Next search result and center" })
+	map("n", "N", "Nzzzv", { desc = "Previous search result and center" })
+	map("n", ";", ":", { noremap = true })
+
+	map("t", "<Esc>", "<C-\\><C-n>")
+
+	map('n', 'ht', ':split | resize -15 | terminal<CR>', { desc = "Open horizontal terminal" })
+	map('n', '<leader>bd', '<CMD>bdelete!<CR>', { desc = "Close current buffer" })
+	map("n", "<leader>pv", "<cmd>Explore<cr>", { desc = "Open file explorer" })
+
+	map("n", "<leader>?", "<cmd>Pick keymaps<CR>", { desc = "Show keymaps" })
+	map("n", "<leader>fd", "<cmd>Pick diagnostics<CR>", { desc = "Show diagnostics" })
+	map("n", "<leader>/", "<cmd>Pick grep_live<cr>", { desc = "Live Grep" })
+	map("n", "<Tab>", "<CMD>Pick buffers<CR>", { desc = "Switch buffers" })
+	map("n", "ff", "<cmd>Pick files<CR>", { desc = "Find files" })
+
+	--==============================================================================
+	-- Plugin Setup
+	--==============================================================================
+	require("rose-pine").setup({ styles = { transparency = true } })
 	vim.cmd.colorscheme "rose-pine"
 
 	require('mason').setup({})
 	require('mason-lspconfig').setup(require('configs.mason'))
 
-
-
-
-
 	require('mini.notify').setup()
 	require("mini.pairs").setup()
 	require("mini.extra").setup()
-	require('mini.statusline').setup {}
+	require('mini.statusline').setup()
 	require("mini.pick").setup {
 		options = { use_cache = true },
-		window = {
-			prompt_prefix = "   ",
-		},
+		window = { prompt_prefix = "   " },
 	}
-
-	require("no-neck-pain").setup {}
-
-	require('blink.cmp').setup(require('configs.cmp'))
-
 	vim.ui.select = MiniPick.ui_select
 
+	require("no-neck-pain").setup {}
+	require('blink.cmp').setup(require('configs.cmp'))
 
-	-- why does it copy when i wanna delete?
-	map({ "n", "x" }, "c", '"_c')
-	map({ "n", "x" }, "<leader>d", '"_d')
+	--==============================================================================
+	-- Autocommands
+	--==============================================================================
+	local autocmd = vim.api.nvim_create_autocmd
+	local augroup = vim.api.nvim_create_augroup
 
-
-	-- telescope
-	vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-	vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-	-- Misc.
-	map("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
-	map("n", ";", ":", { noremap = true })
-
-
-	-- Random stuff
-	map("n", "<C-d>", "<C-d>zz", { desc = "PgDn" })
-	map("n", "<C-u>", "<C-u>zz", { desc = "PgUp" })
-
-	map({ 'n', 'v' }, 'B', '0', { noremap = true })
-	map({ 'n', 'v' }, 'E', '$', { noremap = true })
-
-
-
-	map('n', 'ht', ':split | resize -15 | terminal<CR>', { silent = true })
-
-
-	map('n', '<leader>bd', '<CMD>bdelete!<CR>', { silent = true })
-
-	map("n", "<leader>?", "<cmd>Pick keymaps<CR>", { desc = "keymap help" })
-	map("n", "<leader>fd", "<cmd>Pick diagnostics<CR>", { desc = "Diagnostics" })
-	map("n", "<leader>/", "<cmd>Pick grep_live<cr>", { desc = "Grep" })
-	map("n", "<Tab>", "<CMD>Pick buffers<CR>", { desc = "Tabs" })
-	map("n", "ff", "<cmd>Pick files<CR>", { desc = "Files" })
-
-
-	vim.keymap.set("n", "n", "nzzzv")
-	vim.keymap.set("n", "N", "Nzzzv")
-
-
-
-
-
-
-
+	local lsp_group = augroup('UserLspConfig', {})
+	local file_group = augroup('UserFileConfig', {})
+	local window_group = augroup('UserWindowConfig', {})
 
 	autocmd("BufWritePre", {
-		desc = "Automatically create parent directories if they don't exist when saving a file",
+		group = file_group,
+		desc = "Format on save and ensure parent directory exists",
 		callback = function(args)
-			vim.lsp.buf.format()
-
+			vim.lsp.buf.format({ async = false })
 			local dir = vim.fn.fnamemodify(vim.uv.fs_realpath(args.match) or args.match, ":p:h")
 			if vim.fn.isdirectory(dir) == 0 then
 				vim.fn.mkdir(dir, "p")
@@ -132,28 +135,25 @@ later(function()
 		end,
 	})
 
-
-
-
-	-- Setup autocmd for LSP keymaps
-	vim.api.nvim_create_autocmd("LspAttach", {
+	autocmd("LspAttach", {
+		group = lsp_group,
+		desc = "Setup LSP keymaps on attach",
 		callback = function(ev)
-			map("n", "ca", vim.lsp.buf.code_action, { desc = "LSP: Code Action" })
-
-			map("n", "gD", vim.lsp.buf.declaration, { desc = "LSP: Go to Declaration" })
-			map("n", "gd", vim.lsp.buf.definition, { desc = "LSP: Go to Definition" })
-
-			map("n", "ra", vim.lsp.buf.rename, { desc = "LSP: Rename" })
+			map("n", "ca", vim.lsp.buf.code_action, { buffer = ev.buf, desc = "LSP: Code Action" })
+			map("n", "gD", vim.lsp.buf.declaration, { buffer = ev.buf, desc = "LSP: Go to Declaration" })
+			map("n", "gd", vim.lsp.buf.definition, { buffer = ev.buf, desc = "LSP: Go to Definition" })
+			map("n", "ra", vim.lsp.buf.rename, { buffer = ev.buf, desc = "LSP: Rename" })
 		end,
 	})
 
-
-	vim.api.nvim_create_autocmd("FileType", {
+	autocmd("FileType", {
+		group = window_group,
 		pattern = "help",
 		command = "wincmd L"
 	})
 
-	vim.api.nvim_create_autocmd("BufEnter", {
+	autocmd("BufEnter", {
+		group = window_group,
 		pattern = "*",
 		callback = function()
 			if vim.bo.buftype == "terminal" then
@@ -161,33 +161,4 @@ later(function()
 			end
 		end,
 	})
-
-	map("n", "<leader>pv", "<cmd>Sexplore<cr>", {})
-
-
-	vim.loader.enable()
-	vim.opt.inccommand = "split"
-
-	vim.opt.showtabline = 2
-	vim.opt.cmdheight = 0
-	vim.opt.tabstop = 2
-	vim.opt.shiftwidth = 2
-	vim.opt.laststatus = 3
-	vim.opt.smoothscroll = true
-	vim.opt.smartindent = true
-	vim.opt.ignorecase = true
-	vim.opt.undofile = true
-
-	vim.opt.relativenumber = true
-	vim.opt.number = true
-	vim.opt.signcolumn = "yes"
-
-
-
-	vim.opt.clipboard = "unnamedplus"
-	-- go to previous/next line with h,l,left arrow and right arrow
-	-- when cursor reaches end/beginning of line
-	vim.opt.whichwrap:append("<>[]hl")
-
-	vim.diagnostic.config { virtual_text = true }
 end)
