b7710df82f798b675b74bdb8315f49397ef9888e
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-09-06 16:21:31 -0700

    a

--- init.lua
+++ init.lua
@@ -1,98 +1,91 @@
 vim.g.mapleader = " "
 
-local mini_path = vim.fn.stdpath('data') .. '/site/pack/deps/start/mini.nvim'
-if not vim.loop.fs_stat(mini_path) then
-	vim.fn.system({'git', 'clone', '--filter=blob:none', 'https://github.com/nvim-mini/mini.nvim', mini_path})
-	vim.cmd 'packadd mini.nvim | helptags ALL'
-end
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.inccommand = "split"
+vim.opt.laststatus = 3
+vim.opt.undofile = true
+vim.opt.whichwrap:append("<>[]hl")
+vim.g.loaded_matchparen = 1
 
-require "mini.deps".setup { path = { package = path_package } }
-local add, later = MiniDeps.add, MiniDeps.later
+vim.defer_fn(function()
+	vim.pack.add({
+		{ src = "https://github.com/rose-pine/neovim", name = "rose-pine" },
+		"https://github.com/nvim-mini/mini.nvim",
+		"https://github.com/neovim/nvim-lspconfig",
+		"https://github.com/mason-org/mason.nvim",
+		"https://github.com/mason-org/mason-lspconfig.nvim",
+		"https://github.com/saghen/blink.cmp",
+		"https://github.com/nvim-treesitter/nvim-treesitter",
+		"https://github.com/dstein64/vim-startuptime",
+	})
 
-later(function()
-	add "neovim/nvim-lspconfig"
-	add "mason-org/mason.nvim"
-	add "mason-org/mason-lspconfig.nvim"
-	add "saghen/blink.cmp"
+	require("rose-pine").setup({ styles = { transparency = true } })
+	vim.cmd.colorscheme("rose-pine")
+	vim.diagnostic.config({ virtual_text = true })
 
-	add { source = "rose-pine/neovim", name = "rose-pine" }
-	add "nvim-treesitter/nvim-treesitter"
+	local map = vim.keymap.set
 
-	vim.opt.clipboard = "unnamedplus"
-	vim.opt.cmdheight = 0
-	vim.opt.inccommand = "split"
-	vim.opt.laststatus = 3
-	vim.opt.relativenumber = true
-	vim.opt.shiftwidth = 2
-	vim.opt.tabstop = 2
-	vim.opt.undofile = true
-	vim.opt.whichwrap:append "<>[]hl"
 
-	vim.diagnostic.config { virtual_text = true }
+	map("n", "da", vim.diagnostic.setqflist, { desc = "LSP: Diagnostics" })
+	map("n", "rr", vim.lsp.buf.references, { desc = "LSP: References" })
+	map("n", "S", vim.lsp.buf.document_symbol, { desc = "LSP: Symbols" })
+	map("n", "ca", vim.lsp.buf.code_action, { desc = "LSP: Code Action" })
+	map("n", "gD", vim.lsp.buf.declaration, { desc = "LSP: Go to Declaration" })
+	map("n", "gd", vim.lsp.buf.definition, { desc = "LSP: Go to Definition" })
+	map("n", "ra", vim.lsp.buf.rename, { desc = "LSP: Rename" })
 
-	--==============================================================================
-	-- Keymaps
-	--==============================================================================
-	local map  = function(modes, lhs, rhs, opts)
-		if opts then vim.keymap.set(modes, lhs, rhs, vim.tbl_extend("force", { noremap = true, silent = true }, opts)) end
-	end
-
-	map({ "n", "x" }, "c", "\"_c", { desc = "Cut without yanking" })
+	map({ "n", "x" }, "c", '"_c', { desc = "Cut without yanking" })
 	map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
 	map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
-	map("n", "<C-d>", "<C-d>zz", { desc = "Page down and center" })
-	map("n", "<C-u>", "<C-u>zz", { desc = "Page up and center" })
-
-	map("t", "<Esc>", "<C-\\><C-n>")
-
-	map("n", "<C-/>", ":split | resize -15 | terminal<CR>", { desc = "Open horizontal terminal" })
+	map("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 	map("n", "<leader>bd", "<CMD>lua MiniBufremove.delete()<CR>", { desc = "Close current buffer" })
-	map("n", "<leader>pv", "<cmd>Explore<cr>", { desc = "Open file explorer" })
+	map("n", "!", ":!", {})
+	map("n", "<leader>loc", "<CMD>!cat % | wc -l<CR>", { desc = "Current LOC" })
+	map("n", "<leader>x", "<CMD>!chmod +x %<CR>", { desc = "Make file executable" })
 
-	map("n", "<Tab>", "<CMD>Pick buffers<CR>", { desc = "Switch buffers" })
+	local augroup = vim.api.nvim_create_augroup("UserLspConfig", { clear = true })
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		group = augroup,
+		desc = "Format on save and ensure parent directory exists",
+		callback = function(args)
+			vim.lsp.buf.format({ bufnr = args.buf, timeout_ms = 500 })
+			local dir = vim.fn.fnamemodify(vim.uv.fs_realpath(args.match) or args.match, ":p:h")
+			if not vim.fn.isdirectory(dir) then
+				vim.fn.mkdir(dir, "p")
+			end
+		end,
+	})
 
-	--==============================================================================
-	-- Plugins
-	--==============================================================================
-	require "rose-pine".setup { styles = { transparency = true } }
-	vim.cmd.colorscheme "rose-pine"
+	require("mason").setup({})
+	require("mason-lspconfig").setup(require("configs.mason"))
+	require("nvim-treesitter.configs").setup(require("configs.treesitter"))
+	require("blink.cmp").setup(require("configs.cmp"))
 
-	require "mason".setup {}
-	require "mason-lspconfig".setup(require "configs.mason")
+	for _, name in ipairs({ "tabline", "icons", "bufremove", "surround", "pairs" }) do
+		require("mini." .. name).setup({})
+	end
 
-	require "mini.icons".setup {}
-	require "mini.bufremove".setup {}
-	require "mini.pairs".setup {}
-	require "mini.tabline".setup {}
-
-	require "blink.cmp".setup(require "configs.cmp")
-	require "nvim-treesitter.configs".setup(require "configs.treesitter")
-end)
-
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-	desc = "ensure parent directory exists",
-	callback = function(args)
-		vim.lsp.buf.format()
-		local dir = vim.fn.fnamemodify(vim.uv.fs_realpath(args.match) or args.match, ":p:h")
-		if vim.fn.isdirectory(dir) == 0 then
-			vim.fn.mkdir(dir, "p")
+	map("n", "<leader>?", function()
+		local keymaps = {}
+		local modes = {
+			n = "Normal",
+			i = "Insert",
+			v = "Visual",
+			x = "Visual Block",
+			s = "Select",
+			o = "Operator-pending",
+			t = "Terminal",
+			c = "Command",
+		}
+		for mode, name in pairs(modes) do
+			for _, keymap in ipairs(vim.api.nvim_get_keymap(mode)) do
+				table.insert(keymaps, string.format("%-16s | %-10s | %s", name, keymap.lhs, keymap.rhs))
+			end
 		end
-	end,
-})
-
-vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-	desc = "Setup LSP keymaps on attach",
-	callback = function(e)
-		local map = vim.keymap.set
-		map("n", "da", vim.diagnostic.setqflist, {buffer = e.buf, desc = "LSP: Diagnostics"})
-		map("n", "rr", vim.lsp.buf.references, { buffer = e.buf, desc = "LSP: References" })
-		map("n", "S", vim.lsp.buf.document_symbol, { buffer = e.buf, desc = "LSP: Symbols" })
-		map("n", "ca", vim.lsp.buf.code_action, { buffer = e.buf, desc = "LSP: Code Action" })
-		map("n", "gD", vim.lsp.buf.declaration, { buffer = e.buf, desc = "LSP: Go to Declaration" })
-		map("n", "gd", vim.lsp.buf.definition, { buffer = e.buf, desc = "LSP: Go to Definition" })
-		map("n", "ra", vim.lsp.buf.rename, { buffer = e.buf, desc = "LSP: Rename" })
-	end,
-})
+		vim.fn.setqflist(keymaps, "r", "All Keymaps")
+		vim.cmd("copen")
+	end, { desc = "Show all keymaps" })
+end, 1)
